91fadcc69a2ddb7ca42bf3cef6f9717d528e085e
Xalovald <guilian.bahno@laposte.net>
2024-12-02 11:13:17 +0100

    add sound
    adding code to play sound at the end of timer

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
           CountdownTimer(
             maxTime: 10,
             onTimeUp: () {
-              print('Time is up!');
+              print('Temps écoulé!');
             },
           ),
         ]));
